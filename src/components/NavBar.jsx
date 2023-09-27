82d740b131fd6075b541e753de7036a341ae6b26
Etzshally <shahpaal10@hotmail.com>
2023-09-27 12:55:36 +0500

    `NavBar`

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,48 +1,31 @@
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from "../assets/navitems/LOGO.png"
 
 const NavBar = () => {
     return (
         <>
-            <Navbar expand="md"> {/* Use "md" for a full-width Navbar */}
+            <Navbar sticky="top" expand="md">
                 <Container fluid>
-                    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                    <Navbar.Brand href="#"><img src={logo} alt="logo" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbarScroll" />
                     <Navbar.Collapse id="navbarScroll">
                         <Nav
-                            className="me-auto my-2 my-lg-0"
+                            className="me-auto my-2 my-lg-0 lg:gap-2 xl:gap-14"
                             style={{ maxHeight: '100px' }}
                             navbarScroll
                         >
-                            <Nav.Link href="#action1">Home</Nav.Link>
-                            <Nav.Link href="#action2">Link</Nav.Link>
-                            <NavDropdown title="Link" id="navbarScrollingDropdown">
-                                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action4">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action5">
-                                    Something else here
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Link href="#" disabled>
-                                Link
-                            </Nav.Link>
+                            <Nav.Link href="#action1"><span className='text-xs lg:text-sm font-bold xl:ml-[60px]'>Who we are</span></Nav.Link>
+                            <Nav.Link href="#action2"><span className='text-xs lg:text-sm font-bold'>What we do</span></Nav.Link>
+                            <Nav.Link href="#action3"><span className='text-xs lg:text-sm font-bold'>Energy & Innovation</span></Nav.Link>
+                            <Nav.Link href="#action4"><span className='text-xs lg:text-sm font-bold'>Ecocentury Startups &ventures</span></Nav.Link>
+                            <Nav.Link href="#action5"><span className='text-xs lg:text-sm font-bold'>Resources</span></Nav.Link>
+                            <Nav.Link href="#action6"><span className='text-xs lg:text-sm font-bold'>Store</span></Nav.Link>
                         </Nav>
-                        <Form className="d-flex">
-                            <Form.Control
-                                type="search"
-                                placeholder="Search"
-                                className="me-2"
-                                aria-label="Search"
-                            />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+
+                        <Button className='xl:w-[120px]' variant="success">Login</Button>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
