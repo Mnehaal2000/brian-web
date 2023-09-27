4a723f6f9b6e0e815e0ac84a5b52c2ccc3ad16f2
Etzshally <shahpaal10@hotmail.com>
2023-09-27 13:16:58 +0500

    `SignIn Page`

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import NavBar from "./components/NavBar"
+import SignInPage from './pages/SignInPage'
+// import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <>
-    <NavBar/>
+    {/* <NavBar/> */}
+    <SignInPage/>
     </>
   )
 }
