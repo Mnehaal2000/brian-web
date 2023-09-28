e14c4a714f47379bc1f2415039c2181dfd13b240
Etzshally <shahpaal10@hotmail.com>
2023-09-28 10:38:58 +0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import './App.css'
-import SignUpPage from './pages/SignUpPage'
-// import SignInPage from './pages/SignInPage'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Footer from './components/Footer'
-import NavBar from "./components/NavBar"
+// import SignUpPage from './pages/SignUpPage'
+// // import SignInPage from './pages/SignInPage'
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import Footer from './components/Footer'
+// import NavBar from "./components/NavBar"
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>
-                <NavBar/>
-                <Footer/>
-              </div>,
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <div>
+  //               <NavBar/>
+  //               <Footer/>
+  //             </div>,
+  //   },
+  // ]);
   return (
     <>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
     </>
   )
 }
