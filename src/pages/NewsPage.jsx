e9dd4548fc33a5ea521f578c98ca377624c8c20a
Etzshally <shahpaal10@hotmail.com>
2023-09-28 13:10:17 +0500

    `NewsPage with Pagination and middle out`

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -1,10 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Newsbg from '../assets/newspage/Newsbg.png';
 import Newshead from '../assets/newspage/newshead.png';
 import Newsline from '../assets/newspage/newsline.png';
 import Newscard from '../components/NewsPage/Newscard';
 
 const NewsPage = () => {
+
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const cardsPerPage = 9;
+
+    const newsArticles = [
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+        {
+            category: 'Energy Investment',
+            title: 'Cum socis natoque penatibus et magnis...',
+            description: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec Cum sociis natoque penatibus et magnis dis parturient montes, nascetur',
+            author: 'Nehaal',
+            date: 'July 7, 2023',
+        },
+    ];
+    const startIndex = (currentPage - 1) * cardsPerPage;
+    const endIndex = startIndex + cardsPerPage;
+    const currentNewsArticles = newsArticles.slice(startIndex, endIndex);
+
     return (
         <div className="min-h-screen flex flex-col" style={{ backgroundImage: `url(${Newsbg})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
             <div className="heading p-[2.5rem] mt-20 w-[365px] lg:w-[1000px] m-auto flex flex-col gap-5">
@@ -16,15 +111,23 @@ const NewsPage = () => {
             <div className="flex-grow w-full max-w-6xl mx-auto p-5 lg:p-10 mb-20">
                 <img src={Newsline} alt="" />
                 <div className="grid grid-cols-1 gap-10 lg:grid-cols-3">
-                    <Newscard />
-                    <div className='mt-[-25px]'><Newscard /></div>
-                    <Newscard />
-                    <Newscard />
-                    <div className='mt-[-25px]'><Newscard /></div>
-                    <Newscard />
-                    <Newscard />
-                    <div className='mt-[-25px]'><Newscard /></div>
-                    <Newscard />
+                    {currentNewsArticles.map((article, index) => (
+                        <div key={index} className={index === 1 || index ===4 ||index===7 ? 'mt-[-25px]' : ''}>
+                            <Newscard article={article} />
+                        </div>
+                    ))}
+                </div>
+
+                <div className="flex justify-center mt-4">
+                    {Array.from({ length: Math.ceil(newsArticles.length / cardsPerPage) }).map((_, index) => (
+                        <button
+                            key={index}
+                            className={`mx-2 py-2 px-4 border ${currentPage === index + 1 ? 'bg-green-500 text-white' : 'bg-white text-black'} rounded`}
+                            onClick={() => setCurrentPage(index + 1)}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
                 </div>
             </div>
         </div>
