ec3b7c9203a4fcddce438adaffe785dbe8f48e81
nehaal <mnehaal2000@gmail.com>
2023-10-25 21:58:17 +0500

    faqs and feedbacks added

--- src/components/FeedbackSlider.jsx
+++ src/components/FeedbackSlider.jsx
@@ -21,34 +21,26 @@ export default class FeedbackSlider extends Component {
       <div className=" w-full max-w-[455px] text-center">
         <Slider {...settings}>
             <div className=" lg:pb-0 pb-6 border-b lg:border-b-0 border-gray-300 flex flex-col items-center text-center  gap-5 w-full max-w-[455px] text-xl lg:text-[28px] text-black">
-              <p className=" text-[24px] lg:text-[32px] font-semibold">“ Investing with Impact ”</p>
-              <p className="">“ Ecocentury Energy isn't just about returns, it's about investing with impact. I've found a partner that cares about the planet!! and my portfolio!!! ”</p>
+              <p className=" text-[24px] lg:text-[32px] font-semibold">Investing with Impact </p>
+              <p className="">Ecocentury Energy isn't just about returns, it's about investing with impact. I've found a partner that cares about the planet!! and my portfolio!!! </p>
               <div className="  mt-1 lg:mt-2">
                 <p className=" font-semibold mb-1 lg:mb-3">Nate Springfield</p>
                 <p className=" text-base lg:text-xl">Ecocentury Energy Investor</p>
               </div>
             </div>
             <div className=" lg:pb-0 pb-6 border-b lg:border-b-0 border-gray-300 flex flex-col items-center text-center  gap-5 w-full max-w-[455px] text-xl lg:text-[28px] text-black">
-              <p className=" text-[24px] lg:text-[32px] font-semibold">“ Investing with Impact ”</p>
-              <p className="">“ Ecocentury Energy isn't just about returns, it's about investing with impact. I've found a partner that cares about the planet!! and my portfolio!!! ”</p>
+              <p className=" text-[24px] lg:text-[32px] font-semibold">Impressed by Sustainable Returns</p>
+              <p className="">I invested in Ecocentury Energy seeking both financial returns and a greener future. Their consistent and sustainable returns are exceptionally impressive </p>
               <div className="  mt-1 lg:mt-2">
-                <p className=" font-semibold mb-1 lg:mb-3">Nate Springfield</p>
+                <p className=" font-semibold mb-1 lg:mb-3">Natalie Gillen</p>
                 <p className=" text-base lg:text-xl">Ecocentury Energy Investor</p>
               </div>
             </div>
             <div className=" lg:pb-0 pb-6 border-b lg:border-b-0 border-gray-300 flex flex-col items-center text-center  gap-5 w-full max-w-[455px] text-xl lg:text-[28px] text-black">
-              <p className=" text-[24px] lg:text-[32px] font-semibold">“ Investing with Impact ”</p>
-              <p className="">“ Ecocentury Energy isn't just about returns, it's about investing with impact. I've found a partner that cares about the planet!! and my portfolio!!! ”</p>
+              <p className=" text-[24px] lg:text-[32px] font-semibold">Energizing Investments!</p>
+              <p className="">Investing with Ecocentury Energy feels like plugging into a brighter future. Their returns are not just numbers, they're sparks of innovation</p>
               <div className="  mt-1 lg:mt-2">
-                <p className=" font-semibold mb-1 lg:mb-3">Nate Springfield</p>
-                <p className=" text-base lg:text-xl">Ecocentury Energy Investor</p>
-              </div>
-            </div>
-            <div className=" lg:pb-0 pb-6 border-b lg:border-b-0 border-gray-300 flex flex-col items-center text-center  gap-5 w-full max-w-[455px] text-xl lg:text-[28px] text-black">
-              <p className=" text-[24px] lg:text-[32px] font-semibold">“ Investing with Impact ”</p>
-              <p className="">“ Ecocentury Energy isn't just about returns, it's about investing with impact. I've found a partner that cares about the planet!! and my portfolio!!! ”</p>
-              <div className="  mt-1 lg:mt-2">
-                <p className=" font-semibold mb-1 lg:mb-3">Nate Springfield</p>
+                <p className=" font-semibold mb-1 lg:mb-3">Salina J.</p>
                 <p className=" text-base lg:text-xl">Ecocentury Energy Investor</p>
               </div>
             </div>
